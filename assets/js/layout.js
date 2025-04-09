ef0ee0746add59e3ff797d77123d950ff7226eed
PeggyyOOXX <115986530+PeggyyOOXX@users.noreply.github.com>
2025-04-09 15:16:06 +0800

    Add files via upload

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,3 +1,4 @@
+//手機板側邊欄顯示/隱藏
 const sidebar = document.querySelector(".sidebar");
 const overlay = document.querySelector(".sidebar-overlay");
 const toggleBtn = document.querySelector(".toggle-sidebar");
@@ -14,21 +15,35 @@ sidebarButtons.forEach(button => {
     });
 });
 
+//遊戲入口圖左右滑動
+const scrollControls = document.querySelectorAll('.scroll-controls');
+scrollControls.forEach(control => {
+    const container = control.parentElement.nextElementSibling;
+    if (!container) {
+        return;
+    }
+    const validContainers = ['providers-logoBox', 'game-imgBox'];
+    const isValidContainer = Array.from(container.classList).some(className => validContainers.includes(className));
+    if (!isValidContainer) {
+        return;
+    }
+    const leftBtn = control.querySelector('.scroll-left');
+    const rightBtn = control.querySelector('.scroll-right');
 
+    // 動態計算滑動距離（firstItem的寬度 + 間距）
+    const firstItem = container.querySelector('.providers-logoimgBox') || container.querySelector('.game-entranceimgBox');
+    const scrollDistance = firstItem ? firstItem.offsetWidth * 3 : 0;
+    const updateButtonStates = () => {
+        leftBtn.disabled = container.scrollLeft <= 0;
+        rightBtn.disabled = container.scrollLeft >= container.scrollWidth - container.clientWidth;
+    };
 
-const container = document.querySelector('.providers-logoBox');
-const leftBtn = document.querySelector('.scroll-left');
-const rightBtn = document.querySelector('.scroll-right');
-const scrollDistance = 260 + 500;
-const updateButtonStates = () => {   // 開頭/結尾時禁用按鈕
-    leftBtn.disabled = container.scrollLeft <= 0;
-    rightBtn.disabled = container.scrollLeft >= container.scrollWidth - container.clientWidth;
-};
-updateButtonStates();
-leftBtn.addEventListener('click', () => {   // 向左滑動
-    container.scrollLeft -= scrollDistance;
+    updateButtonStates();
+    leftBtn.addEventListener('click', () => {   // 向左滑動
+        container.scrollLeft -= scrollDistance;
+    });
+    rightBtn.addEventListener('click', () => {   // 向右滑動
+        container.scrollLeft += scrollDistance;
+    });
+    container.addEventListener('scroll', updateButtonStates);   // 監聽滾動
 });
-rightBtn.addEventListener('click', () => {   // 向右滑動
-    container.scrollLeft += scrollDistance;
-});
-container.addEventListener('scroll', updateButtonStates);   // 監聽滾動
