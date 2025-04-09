a0f0c3230bee7807ba4c3e9485c06db4d621d5f2
PeggyyOOXX <115986530+PeggyyOOXX@users.noreply.github.com>
2025-04-09 10:25:31 +0800

    Add files via upload

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -12,4 +12,23 @@ sidebarButtons.forEach(button => {
     button.addEventListener("click", () => {
       sidebar.classList.remove("show");
     });
-  });
+});
+
+
+
+const container = document.querySelector('.providers-logoBox');
+const leftBtn = document.querySelector('.scroll-left');
+const rightBtn = document.querySelector('.scroll-right');
+const scrollDistance = 260 + 500;
+const updateButtonStates = () => {   // 開頭/結尾時禁用按鈕
+    leftBtn.disabled = container.scrollLeft <= 0;
+    rightBtn.disabled = container.scrollLeft >= container.scrollWidth - container.clientWidth;
+};
+updateButtonStates();
+leftBtn.addEventListener('click', () => {   // 向左滑動
+    container.scrollLeft -= scrollDistance;
+});
+rightBtn.addEventListener('click', () => {   // 向右滑動
+    container.scrollLeft += scrollDistance;
+});
+container.addEventListener('scroll', updateButtonStates);   // 監聽滾動
